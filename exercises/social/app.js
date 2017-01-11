59eb81bf4c2a2b7234969639be0a9139133d94e3
Mike <mikedavid364@gmail.com>
2017-01-11 12:24:46 -0800

    Added to newsies and social.js

--- exercises/social/app.js
+++ exercises/social/app.js
@@ -2,7 +2,7 @@ var snowboarder = {
     name: "mike",
     age: 38,
     agePerYear: function(){
-        age++;
+        this.age++;
         return this.age;
     },
     favoriteMountains:[
@@ -49,7 +49,8 @@ snowboarder.favoriteMountains[1].price = "expensive";
 var vail = {mountainName: "Vail", 
             county: "Summit", 
             state: "Colorado"};
+
 snowboarder.favoriteMountains.push({vail});
 console.log(snowboarder.favoriteMountains[2].attributes.avgSnowfall);
 console.log(snowboarder.favoriteMountains[2].attributes.colorSnow);
-console.log(snowboarder);
+console.log(snowboarder.agePerYear());
